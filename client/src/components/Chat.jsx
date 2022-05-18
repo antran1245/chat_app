d0fff56e715fc8670109296799153cf1df40f1fa
antran1245 <antran1245@gmail.com>
2022-05-17 22:15:08 -0700

    added a emit to message

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { SocketContext } from "../context/SocketContext";
 import Form from "./Form";
 
 export default (props) => {
     const [inputMessage, setInputMessage] = useState('')
+    const {socket} = useContext(SocketContext);
+    
     const handleMessage = () => {
         console.log(inputMessage)
+        socket.emit('chat message', inputMessage)
     }
+
+
     
     return(
         <div className="box chatBox">
